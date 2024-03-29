cbcad0eb3a3162f0c157b673d08bf6e369394299
jang_jiwon <kooriangman@gmail.com>
2024-03-29 13:03:51 +0900

    [INIT] 초기 세팅

---
+++ cicd/src/main/java/accc/cicd/controller/CicdController.java
@@ -0,0 +1,13 @@
+package accc.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CicdController{
+	
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "Hello World";
+	}
+}
